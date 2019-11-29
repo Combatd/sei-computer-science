d992d3af20bc6b3fb596737b27c6742a71771b56
Combatd <combatdstar@gmail.com>
2019-11-28 16:46:23 -0800

    add recursion iterating index in sumArrayOfNums

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -5,5 +5,6 @@ function sumArrayOfNums(arr, index = 0, sum = 0) {
     }
     // step 3: action step - add number at current index of arr to sum
     sum += arr[index];
-
+    // step 4: recursion - iterate the index while invoking sumArrayOfNums
+    return sumArrayOfNums(arr, index + 1, sum);
 }
